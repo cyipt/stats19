7da475d6c49ae41f07b141a83f4a209c29c6e66f
Robin Lovelace <rob00x@gmail.com>
2018-09-28 15:51:09 +0100

    Update repo

--- R/download_functions.R
+++
@@ -1,6 +0,0 @@
-stats19.download <- function(type = c("acc","cas","veh"),year){
-  #Get List of Datasets from Data.GOV.UK
-  link <- "https://data.gov.uk/api/3/action/package_show?id=road-accidents-safety-data"
-  dir <- jsonlite::fromJSON(link, simplifyVector = FALSE)
-
-}

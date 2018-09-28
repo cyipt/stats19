781d92e1d959e940f88a8a0acce80083b49dae5d
Robin Lovelace <rob00x@gmail.com>
2018-09-28 15:59:32 +0100

    Add example dataset

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Sample stats19 dataset
+#'
+#' Todo: add vehicles and casualties datasets
+#'
+#' @examples
+#' \dontrun{
+#' set.seed(2018)
+#' ac_sample1000 = ac[sample(nrow(ac), 1000), ]
+#' usethis::use_data(ac_sample1000, overwrite = TRUE)
+#' }
+#' plot(ac_sample1000$Longitude, ac_sample1000$Latitude)
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ac_sample1000
+#' @format A data frame
+NULL
